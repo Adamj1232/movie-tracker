540e65e52249513d929b3fda22d028b7f64fa6da
Jhun de Andres <joshuajhun@gmail.com>
2017-05-15 13:40:26 -0600

    Application is at a bare minum state. disconnected router 3 and removed bundle

--- app/index.js
+++ app/index.js
@@ -2,14 +2,5 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
 import MovieIndex from './components/movieIndex';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
-const router = (
-    <Router history={browserHistory} >
-      <Route path='/' component={App}>
-        <IndexRoute component={MovieIndex} />
-      </Route>
-    </Router>
-)
-
-ReactDOM.render(router, document.getElementById('main'))
+ReactDOM.render(App, document.getElementById('main'))
