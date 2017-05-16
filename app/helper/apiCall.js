842cb0f2ab67c6ec1223146f601d1769a2d5aed7
Adam Carpenter <adamcarpenter86@gmail.com>
2017-05-16 15:01:38 -0600

    basic setup of file structure

--- app/helper/apiCall.js
+++ app/helper/apiCall.js
@@ -1,5 +1,7 @@
+import apiKey from '../key.js'
+
 export const fetchMovies = () => {
-  const movieApi = `https://api.themoviedb.org/3/movie/now_playing?api_key=de2f6f839f875c177539f24f874dc62e&language=en-US&page=1`
+  const movieApi = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&page=1`
 
   fetch(movieApi)
   .then((resp) => {
