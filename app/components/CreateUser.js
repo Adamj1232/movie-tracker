679340e8ff0e01a4f526336e9a6ba1a8ff88b4cd
Jack Bevis <jack.bevis@gmail.com>
2017-05-17 16:45:31 -0600

    create new user component built with post request to database firing

--- app/components/CreateUser.js
+++ app/components/CreateUser.js
@@ -2,17 +2,60 @@ import React, { Component } from 'react';
 
 export default class CreateUser extends Component {
   constructor() {
-    super();
+    super()
+    this.state = {
+      name: '',
+      email: '',
+      password: ''
+    }
   }
+
+  addNewUser(e) {
+    console.log('add user fired')
+    e.preventDefault()
+    const { name, email, password } = this.state;
+
+    fetch('/api/users/new', {
+      method: 'POST',
+      headers: {'Content-Type':'application/json'},
+      body: JSON.stringify({name, email, password})
+    })
+    .catch((error) => {
+      alert('User already exists')
+      console.log(error, 'User already exists')
+    })
+    this.setState({
+                    name: '',
+                    email: '',
+                    password: ''
+                  })
+  }
+
 
   render() {
     return (
-      <section className = 'create-user-controls'>
-        <input placeholder='please enter name'/>
-        <input placeholder='please enter e-mail'/>
-        <input placeholder='please enter password'/>
-        <button className = 'login-submit'>Submit</button>
-      </section>
+      <form className = 'create-user-controls'>
+        <input type='text'
+               placeholder='please enter name'
+               value={this.state.name}
+               onChange={(e) => {
+                 this.setState({ name: e.target.value })}} />
+        <input type='text'
+               placeholder='please enter email'
+               value={this.state.email}
+               onChange={(e) => {
+                 this.setState({ email: e.target.value })}} />
+        <input type='text'
+               placeholder='please enter password'
+               value={this.state.password}
+               onChange={(e) => {
+                 this.setState({ password: e.target.value })}} />
+        <button className = 'login-submit'
+                onClick={(e) => {
+                  this.addNewUser(e)
+                }}
+          >Submit</button>
+      </form>
 
     )
   }
