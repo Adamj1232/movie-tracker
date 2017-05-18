ad99aed4767aca3855e8327c0af6d0e5738723f4
Adam Carpenter <adamcarpenter86@gmail.com>
2017-05-18 10:01:41 -0600

    basic stylings and added password hidden

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -3,6 +3,7 @@ import React from 'react';
 export const NavBar = () => {
   return (
     <section className="nav-bar">
+      <h1> Movie</h1><h1 className='tracker'>Tracker</h1>
       <button className='login-button'>Login</button>
       <button className='account-button'>Create Account</button>
       <button className='fav-button'>Favorites</button>
