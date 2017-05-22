a661be147546981f5143260f47478adc003abfc7
Breana Knight <Akewlbreeze.bc@gmail.com>
2017-05-22 09:16:01 -0600

    adjustments to movieCard, favoritesCard and some other shit

--- test/components/MovieCard.test.js
+++ test/components/MovieCard.test.js
@@ -3,23 +3,48 @@ import { mount, shallow } from 'enzyme';
 import  {MovieCard} from '../../app/components/MovieCard.js';
 import expect from 'expect';
 
-describe('FavoriteCard component',()=>{
+describe('MovieCard component',()=>{
 
-  it('should display a favorites button',()=>{
 
-    const wrapper = shallow(
-      <MovieCard />)
+    it('should display a favorites button',()=>{
 
-      console.log(wrapper)
+      const wrapper = shallow(
+        <MovieCard    favorites={{}}
+                      handleDeleteFave={()=>{}}
+                      movie_id={'test_movieId'}
+                      overview={'test_overview'}
+                      poster_path={'test_posterPath'}
+                      release_date={'test_release_date'}
+                      title={'test_title'}
+                      updateFavorites={()=>{}}
+                      user_id={1}
+        />)
+        console.log(wrapper.debug())
+        expect(wrapper.find('button').length).toEqual(1)
+        expect(wrapper.find('button').props().className).toEqual('unfavorited')
 
-      //  expect(wrapper.find('.button').first().props().children).toEqual('Login')
-      //  expect(wrapper.find('.button').first().props().to).toEqual('/Login')
+
+
+        //  expect(wrapper.find('.button').first().props().children).toEqual('Login')
+        //  expect(wrapper.find('.button').first().props().to).toEqual('/Login')
+
+       })
+
+       it('should dispaly an image of favorited movie',()=>{
+
+         const wrapper = shallow(
+           <MovieCard favorites={{}}
+                         handleDeleteFave={()=>{}}
+                         movie_id={'test_movieId'}
+                         overview={'test_overview'}
+                         poster_path={'test_posterPath'}
+                         release_date={'test_release_date'}
+                         title={'test_title'}
+                         updateFavorites={()=>{}}
+                         user_id={1}
+           />)
+           console.log(wrapper.debug())
+           expect(wrapper.find('.movie-poster').length).toEqual(1)
+       })
 
      })
-
-  it('should render movie information', ()=>{
-
-  })
-
-
-   })
